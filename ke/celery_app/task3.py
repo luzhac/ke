dbdb534906733593c62f046dae6b299a38c98c2d
luzhac <luzhac@gmail.com>
2018-04-30 13:13:38 +0100

    task3

--- ke/celery_app/task3.py
+++ ke/celery_app/task3.py
@@ -4,18 +4,18 @@ import os
 import pymongo
 
 
-#client = pymongo.MongoClient(host='localhost', port=27017)
-#db = client['keqq']
-#collection = db['ke']
+client = pymongo.MongoClient(host='localhost', port=27017)
+db = client['ke']
+collection = db['ke']
 
 
 @app.task()
 def crawl1():
     a=0
     print('asdfadsfadsfadsf')
-    #result = collection.find_one(sort=[("batch", -1)])
+    result = collection.find_one(sort=[("batch", -1)])
 
-    #a=int(result['batch'])+1
+    a=int(result['batch'])+1
 
     os.system("scrapy crawl ke -a arg="+str(a))
     os.system("scrapy crawl study163 -a arg="+str(a))
